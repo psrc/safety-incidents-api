6dd612058e5c781d8c43b16bbe2a992e72d50130
christopeak <cpeak@psrc.org>
2024-11-05 07:11:33 -0800

    add stub api

---
+++ main/incident_api.py
@@ -0,0 +1,57 @@
+
+from fastapi import FastAPI, HTTPException
+import psrcelmerpy
+import pandas as pd
+import sqlite3
+
+app  = FastAPI()
+
+e_conn = psrcelmerpy.ElmerConn()
+s_conn = sqlite3.connect("safety.sqlite")
+
+@app.get("/")
+def root():
+    return {"message": "Hello world"}
+
+    
+@app.get("/vehicles/{incident_rec_id}")
+async def get_vehicles_by_id(incident_rec_id: int):
+    qry = f"""
+        select i.Collision_report_number, v.Vehicle_Type 
+        from incidents as i 
+            join vehicles as v on i.incident_rec_id = v.incident_rec_id 
+        where i.incident_rec_id = {incident_rec_id}
+        """
+    # df = e_conn.get_query(qry)
+    df = pd.read_sql(qry, s_conn)
+    return df.to_json(force_ascii=True)
+    
+@app.get("/persons/{incident_rec_id}")
+async def get_persons_by_id(incident_rec_id: int):
+    try:
+
+        qry_incident = f"""
+            select i.Collision_report_number, i.incident_date
+            from incidents as i
+            where i.incident_rec_id = {incident_rec_id}
+            """
+        df_incident = pd.read_sql(qry_incident, s_conn)
+        
+        qry_person = f"""
+            select p.Involved_Person_Type, p.Age, p.Gender
+            from incidents as i 
+                join vehicles as v on i.incident_rec_id = v.incident_rec_id 
+                join persons as p on v.vehicle_rec_id = p.vehicle_rec_id
+            where i.incident_rec_id = {incident_rec_id}
+            """
+        # df = e_conn.get_query(qry)
+        df_person = pd.read_sql(qry_person, s_conn)
+
+        structured_data = {
+            'incident': df_incident.to_dict(orient='records')[0],
+            'persons': df_person.to_dict(orient='records')
+        }
+        return structured_data
+    
+    except:
+        print("Something went wrong in get_person_by_id")
