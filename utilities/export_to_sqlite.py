86885f4feaa2d8d12ac7686d954b4220636d203b
christopeak <cpeak@psrc.org>
2024-11-05 07:14:08 -0800

    add script to export data to a local sqlite db

---
+++ utilities/export_to_sqlite.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import psrcelmerpy
+import sqlite3 
+
+sqlite_path = "../main/safety.sqlite"
+
+e_conn = psrcelmerpy.ElmerConn()
+sqlite_conn =  sqlite3.connect(sqlite_path)
+
+tbls = ['incidents', 'vehicles', 'persons']
+# tbl_nm = 'vehicles'
+for tbl_nm in tbls:
+    # out_tbl = f"safety.{tbl_nm}"
+    query = f"SELECT * FROM safety.{tbl_nm}"
+    df = e_conn.get_query(query)
+    df.to_sql(tbl_nm, sqlite_conn, if_exists='replace', index=False)
+    rows_processed = len(df)
+    print(f"Processed {rows_processed} rows for {tbl_nm}")
+
+sqlite_conn.close()
