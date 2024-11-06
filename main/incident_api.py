a7c1b868a3e0c6116be315d25bce5e5ed5844921
christopeak <cpeak@psrc.org>
2024-11-06 10:40:09 -0800

    fix formatting in get_vehicles_by_id()

--- main/incident_api.py
+++ main/incident_api.py
@@ -12,26 +12,44 @@ def root():
     return {"message": "WSDOT Safety Incident Data API"}
 
     
-@app.get("/vehicles/{incident_rec_id}")
-async def get_vehicles_by_id(incident_rec_id: int):
-    qry = f"""
-        select i.Collision_report_number, v.Vehicle_Type 
-        from incidents as i 
-            join vehicles as v on i.incident_rec_id = v.incident_rec_id 
-        where i.incident_rec_id = {incident_rec_id}
-        """
-    df = pd.read_sql(qry, s_conn)
-    return df.to_json(force_ascii=True)
+@app.get("/vehicles/{collision_report_number}")
+async def get_vehicles_by_id(collision_report_number: str):
+
+    try:
+        qry = f"""
+                select i.Collision_report_number, i.incident_date
+                from incidents as i
+                where i.Collision_Report_number = '{collision_report_number}'
+            """
+        df_incident = pd.read_sql(qry, s_conn)
+        
+        qry = f"""
+            select v.unit_number, v.Vehicle_Type 
+            from incidents as i 
+                join vehicles as v on i.incident_rec_id = v.incident_rec_id 
+                where i.Collision_Report_number = '{collision_report_number}'
+            """
+        df_vehicles = pd.read_sql(qry, s_conn)
+        
+        structured_data = {
+            'incident': df_incident.to_dict(orient='records')[0],
+            'vehicles': df_vehicles.to_dict(orient='records')
+        }
+        
+        return structured_data
+
+    except:
+        print("Something went wrong in get_vehicles_by_id")
     
 
-@app.get("/persons/{incident_rec_id}")
-async def get_persons_by_id(incident_rec_id: int):
+@app.get("/persons/{collision_report_number}")
+async def get_persons_by_id(collision_report_number: str):
     try:
 
         qry_incident = f"""
             select i.Collision_report_number, i.incident_date
             from incidents as i
-            where i.incident_rec_id = {incident_rec_id}
+            where i.Collision_Report_number = '{collision_report_number}'
             """
         df_incident = pd.read_sql(qry_incident, s_conn)
         
@@ -40,7 +58,7 @@ async def get_persons_by_id(incident_rec_id: int):
             from incidents as i 
                 join vehicles as v on i.incident_rec_id = v.incident_rec_id 
                 join persons as p on v.vehicle_rec_id = p.vehicle_rec_id
-            where i.incident_rec_id = {incident_rec_id}
+            where i.Collision_Report_number = '{collision_report_number}'
             """
         df_person = pd.read_sql(qry_person, s_conn)
 
