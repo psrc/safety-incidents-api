1e29d1f64a6ea20d9d321b20369b8ad2d6077b1f
stefancoe <coestefan@gmail.com>
2024-12-03 11:15:47 -0800

    Using SQLModel example.

--- main/incident_api_orm.py
+++ main/incident_api_orm.py
@@ -1,22 +1,38 @@
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
 
-class Incidents(SQLModel, table=True):
+class Incident(SQLModel, table=True):
     Collision_Report_Number: str = Field(primary_key=True)
     Latitude: float = Field()
     Longitude: float = Field()
     City_Name: str = Field()
     County_Name: str = Field()
+    
+    vehicles: list["Vehicle"] = Relationship(back_populates="incident")
 
-class Vehicles(SQLModel, table=True):
+class Vehicle(SQLModel, table=True):
+    vehicle_rec_id: int = Field(primary_key=True)
     #vehicle_rec_id: int = Field(primary_key=True)
-    Collision_Report_Number: str = Field(primary_key=True)
+    Collision_Report_Number: str = Field(foreign_key="incident.Collision_Report_Number")
+    #Collision_Report_Number: str = Field(primary_key=True)
     Vehicle_Type: str = Field()
     Vehicle_Make: str = Field()
     Vehicle_Model: str = Field()
     Vehicle_Style: str = Field()
+    #team_id: int | None = Field(default=None, foreign_key="team.id")
+    incident: Incident | None = Relationship(back_populates="vehicles")
+    #incident_id: int | None = Field(default=None, foreign_key="Incident.Collision_Report_Number")
+
+
+# class VehicleWithIncident(Vehicle):
+#     incident: Incident | None = None 
+
+# class IncidentWithVehicles(Incident):
+#     vehicle: list[Vehicle] = []
+
+
 
 # class Persons(SQLModel, table=True):
 #     person_rec_id: int = Field(primary_key=True)
@@ -47,34 +63,40 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-@app.get("/incidents/")
-def read_incidents(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Incidents]:
-    incidents = session.exec(select(Incidents).offset(offset).limit(limit)).all()
-    return incidents
+# @app.get("/incidents/")
+# def read_incidents(
+#     session: SessionDep,
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[Incidents]:
+#     incidents = session.exec(select(Incidents).offset(offset).limit(limit)).all()
+#     return incidents
 
-@app.get("/incidents/{Collision_Report_Number}")
-def read_incident(Collision_Report_Number: str, session: SessionDep) -> Incidents:
-    incident = session.get(Incidents, Collision_Report_Number)
-    if not incident:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    return incident
+@app.get("/incidents/", response_model=list[Incident])
+def read_incidents(offset: int = 0, limit: int = Query(default=100, le=100)):
+    with Session(engine) as session:
+        incidents = session.exec(select(Incident).offset(offset).limit(limit)).all()
+        return incidents
+    
 
-@app.get("/vehicles/")
-def read_vehicles(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Vehicles]:
-    vehicles = session.exec(select(Vehicles).offset(offset).limit(limit)).all()
-    return vehicles
+@app.get("/incidents/{Collision_Report_Number}", response_model=Incident)
+def read_incident(Collision_Report_Number: str):
+    with Session(engine) as session:
+        incident = session.get(Incident, Collision_Report_Number)
+        if not incident:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        return incident
 
-@app.get("/vehicles/{Report_Number}")
-def read_vehicle(Report_Number: str, session: SessionDep) -> Vehicles:
-    vehicle = session.get(Vehicles, Report_Number)
-    if not vehicle:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    return vehicle
+@app.get("/vehicles/", response_model=list[Vehicle])
+def read_vehicles(offset: int = 0, limit: int = Query(default=100, le=100)):
+    with Session(engine) as session:
+        vehicles = session.exec(select(Vehicle).offset(offset).limit(limit)).all()
+        return vehicles
+
+@app.get("/vehicles/{vehicle_rec_id}", response_model=Vehicle)
+def read_vehicle(vehicle_rec_id: str):
+    with Session(engine) as session:
+        vehicle = session.get(Vehicle,  vehicle_rec_id)
+        if not vehicle:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        return vehicle
--- utilities/export_to_sqlite.py
+++ utilities/export_to_sqlite.py
@@ -7,16 +7,17 @@ sqlite_path = "./main/safety2.sqlite"
 e_conn = psrcelmerpy.ElmerConn()
 sqlite_conn =  sqlite3.connect(sqlite_path)
 
-tbls = ['incidents', 'vehicles', 'persons']
+#tbls = ['incidents', 'vehicles', 'persons']
+tbls = {'incidents' : 'incident', 'vehicles' : 'vehicle', 'persons' : 'person'}
 # tbl_nm = 'vehicles'
-for tbl_nm in tbls:
+for tbl_nm in tbls.keys():
     # out_tbl = f"safety.{tbl_nm}"
     query = f"""
         SELECT * 
         FROM safety.{tbl_nm} 
     """
     df = e_conn.get_query(query)
-    df.to_sql(tbl_nm.capitalize(), sqlite_conn, if_exists='replace', index=False)
+    df.to_sql(tbls[tbl_nm].capitalize(), sqlite_conn, if_exists='replace', index=False)
     rows_processed = len(df)
     print(f"Processed {rows_processed} rows for {tbl_nm}")
 
