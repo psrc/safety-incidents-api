229b31d5b246a81405545b936c29d43a612a94e3
christopeak <cpeak@psrc.org>
2024-12-06 10:27:08 -0800

    minimal working api using sqlmodel

--- main/incident_api.py
+++ main/incident_api.py
@@ -1,89 +1,131 @@
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Query
+from typing import Optional, Dict, Type
 import pandas as pd
 import sqlite3
+from typing import Annotated
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, inspect, MetaData, Table
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
 
-app  = FastAPI()
 
 
 def get_db_connection():
-    return sqlite3.connect("safety.sqlite")
+    sqlite_file_name = "safety4.sqlite"
+    sqlite_url = f"sqlite:///{sqlite_file_name}?mode=ro"
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(sqlite_url, connect_args=connect_args)
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    return engine, metadata
 
+def get_pk(table_name: str):
+    """
+    return the primary key column for a table
+    """
+    pk_dict = {
+        'incident': 'incident_rec_id',
+        'vehicle': 'vehicle_rec_id',
+        'person': 'person_rec_id'
+    }
+    return pk_dict[table_name]
+
+
+engine, metadata = get_db_connection()
+
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_session():
+    #session = SessionLocal()
+    try:
+        with Session(engine) as session:
+            yield session
+    finally:
+        session.close()
+
+
+class Incident(SQLModel, table=True):
+    Collision_Report_Number: str = Field(primary_key=True)
+    incident_date: str = Field()
+
+
+app  = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "WSDOT Safety Incident Data API"}
 
+@app.get("/incidents/{Collision_Report_Number}", response_model=Incident)
+async def get_incident(Collision_Report_Number: str):
+    # Incident = models['incidents']
+    with Session(engine) as session:
+        incidents = session.get(Incident, Collision_Report_Number)
+        if not incidents:
+            raise HTTPException(status_code=404, detail="No Incidents found")
+        return incidents
 
-@app.get("/incidents")
-async def get_incidents():
-    with get_db_connection() as s_conn:
-        qry = """
-            select i.Collision_report_number, i.incident_date
-            from incidents as i
-        """
-        df = pd.read_sql(qry, s_conn)
-        return df.to_dict(orient='records')
+
     
     
-@app.get("/vehicles/{collision_report_number}")
-async def get_vehicles_by_id(collision_report_number: str):
+# @app.get("/vehicles/{collision_report_number}")
+# async def get_vehicles_by_id(collision_report_number: str):
 
-    with get_db_connection() as s_conn:
-        try:
-            qry = f"""
-                    select i.Collision_report_number, i.incident_date
-                    from incidents as i
-                    where i.Collision_Report_number = ?
-                """
-            df_incident = pd.read_sql(qry, s_conn, params=[collision_report_number])
+#     with get_db_connection() as s_conn:
+#         try:
+#             qry = f"""
+#                     select i.Collision_report_number, i.incident_date
+#                     from incidents as i
+#                     where i.Collision_Report_number = ?
+#                 """
+#             df_incident = pd.read_sql(qry, s_conn, params=[collision_report_number])
             
-            qry = f"""
-                select v.unit_number, v.Vehicle_Type 
-                from incidents as i 
-                    join vehicles as v on i.incident_rec_id = v.incident_rec_id 
-                    where i.Collision_Report_number = ?
-                """
-            df_vehicles = pd.read_sql(qry, s_conn, params=[collision_report_number])
+#             qry = f"""
+#                 select v.unit_number, v.Vehicle_Type 
+#                 from incidents as i 
+#                     join vehicles as v on i.incident_rec_id = v.incident_rec_id 
+#                     where i.Collision_Report_number = ?
+#                 """
+#             df_vehicles = pd.read_sql(qry, s_conn, params=[collision_report_number])
             
-            structured_data = {
-                'incident': df_incident.to_dict(orient='records')[0],
-                'vehicles': df_vehicles.to_dict(orient='records')
-            }
+#             structured_data = {
+#                 'incident': df_incident.to_dict(orient='records')[0],
+#                 'vehicles': df_vehicles.to_dict(orient='records')
+#             }
             
-            return structured_data
+#             return structured_data
 
-        except Exception as e:
-            raise HTTPException(status_code=500, detail="Error retrieving vehicle data")
+#         except Exception as e:
+#             raise HTTPException(status_code=500, detail="Error retrieving vehicle data")
     
 
-@app.get("/persons/{collision_report_number}")
-async def get_persons_by_id(collision_report_number: str):
+# @app.get("/persons/{collision_report_number}")
+# async def get_persons_by_id(collision_report_number: str):
 
-    with get_db_connection() as s_conn:
-        try:
+#     with get_db_connection() as s_conn:
+#         try:
 
-            qry_incident = f"""
-                select i.Collision_report_number, i.incident_date
-                from incidents as i
-                where i.Collision_Report_number = ?
-                """
-            df_incident = pd.read_sql(qry_incident, s_conn, params=[collision_report_number])
+#             qry_incident = f"""
+#                 select i.Collision_report_number, i.incident_date
+#                 from incidents as i
+#                 where i.Collision_Report_number = ?
+#                 """
+#             df_incident = pd.read_sql(qry_incident, s_conn, params=[collision_report_number])
             
-            qry_person = f"""
-                select p.Involved_Person_Type, p.Age, p.Gender
-                from incidents as i 
-                    join vehicles as v on i.incident_rec_id = v.incident_rec_id 
-                    join persons as p on v.vehicle_rec_id = p.vehicle_rec_id
-                where i.Collision_Report_number = ?
-                """
-            df_person = pd.read_sql(qry_person, s_conn, params=[collision_report_number])
+#             qry_person = f"""
+#                 select p.Involved_Person_Type, p.Age, p.Gender
+#                 from incidents as i 
+#                     join vehicles as v on i.incident_rec_id = v.incident_rec_id 
+#                     join persons as p on v.vehicle_rec_id = p.vehicle_rec_id
+#                 where i.Collision_Report_number = ?
+#                 """
+#             df_person = pd.read_sql(qry_person, s_conn, params=[collision_report_number])
 
-            structured_data = {
-                'incident': df_incident.to_dict(orient='records')[0],
-                'persons': df_person.to_dict(orient='records')
-            }
-            return structured_data
+#             structured_data = {
+#                 'incident': df_incident.to_dict(orient='records')[0],
+#                 'persons': df_person.to_dict(orient='records')
+#             }
+#             return structured_data
         
-        except Exception as e:
-            raise HTTPException(status_code=500, detail="Error retrieving person data")
+#         except Exception as e:
+#             raise HTTPException(status_code=500, detail="Error retrieving person data")
--- utilities/export_to_sqlite.py
+++ utilities/export_to_sqlite.py
@@ -2,24 +2,53 @@ import pandas as pd
 import psrcelmerpy
 import sqlite3 
 
-sqlite_path = "./main/safety2.sqlite"
+sqlite_path = "./main/safety4.sqlite"
 
 e_conn = psrcelmerpy.ElmerConn()
 sqlite_conn =  sqlite3.connect(sqlite_path)
 
-tbls = ['incidents', 'vehicles', 'persons']
-# tbl_nm = 'vehicles'
-for tbl_nm in tbls:
-    # out_tbl = f"safety.{tbl_nm}"
-    query = f"""
-        SELECT * 
-        FROM safety.{tbl_nm} 
-    """
-    df = e_conn.get_query(query)
-    df.to_sql(tbl_nm, sqlite_conn, if_exists='replace', index=False)
-    rows_processed = len(df)
-    print(f"Processed {rows_processed} rows for {tbl_nm}")
+# tbls = ['incidents', 'vehicles', 'persons']
 
-print(f"export completed for {sqlite_path}")
+tbls = {
+    'incident': ['incident_rec_id', 'Collision_Report_Number', 'incident_date'],
+    'vehicle': ['vehicle_rec_id', 'unit_number', 'Vehicle_Type'],
+    'person': ['person_rec_id', 'Involved_Person_Type', 'Age', 'Gender']
+}
+
+primary_keys = {
+    'incident': 'incident_rec_id',
+    'vehicle': 'vehicle_rec_id',
+    'person': 'person_rec_id'
+}
+
+source_tables = {'incident': 'Incidents', 'vehicle': 'Vehicles', 'person': 'Persons'}
+
+# tbl_nm = 'vehicles'
+def export():
+    try: 
+        print("entered export")
+        pks = primary_keys
+        for tbl_nm in tbls.keys():
+            # out_tbl = f"safety.{tbl_nm}"
+            t_nm = source_tables[tbl_nm]
+            columns = ','.join(tbls[tbl_nm])
+            query = f"""
+                SELECT {columns}
+                FROM safety.{t_nm} 
+            """
+            print(query)
+            df = e_conn.get_query(query)
+            # dtype_clause = f"'{pks[tbl_nm]}': 'INTEGER PRIMARY KEY AUTOINCREMENT'"
+            #df.to_sql(tbl_nm, sqlite_conn, if_exists='replace', index=False, dtype={pks[tbl_nm]: 'INTEGER PRIMARY KEY AUTOINCREMENT'})
+            df.to_sql(tbl_nm, sqlite_conn, if_exists='replace', index=False)
+            rows_processed = len(df)
+            print(f"Processed {rows_processed} rows for {tbl_nm}")
+
+        print(f"export completed for {sqlite_path}")
+
+    except Exception as e:
+        print(f"exception in export routine: {e.args[0]}")
+
+export()
 
 sqlite_conn.close()
