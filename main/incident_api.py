da521405a55cac60db5be4a5c78251f26057d4bc
christopeak <cpeak@psrc.org>
2025-02-13 09:31:17 -0800

    /incident_by_city/{city_name} works for a single returned row, not for a set of incidents

--- main/incident_api.py
+++ main/incident_api.py
@@ -1,22 +1,22 @@
 
 from fastapi import Depends, FastAPI, HTTPException, Query
-from typing import Optional, Dict, Type, List
-import pandas as pd
-import sqlite3
-from typing import Annotated
-from sqlalchemy import Column, Integer, String, DateTime, create_engine, inspect, MetaData, Table
-from sqlalchemy.orm import sessionmaker
+# from typing import Optional, Dict, Type, List
+# import pandas as pd
+# import sqlite3
+# from typing import Annotated
+# from sqlalchemy import Column, Integer, String, DateTime, create_engine, inspect, MetaData, Table
+#from sqlalchemy.orm import sessionmaker
 from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
 
 
 def get_db_connection():
-    sqlite_file_name = "safety.sqlite"
+    sqlite_file_name = "safety2.sqlite"
     sqlite_url = f"sqlite:///{sqlite_file_name}?mode=ro"
     connect_args = {"check_same_thread": False}
     engine = create_engine(sqlite_url, connect_args=connect_args)
-    metadata = MetaData()
-    metadata.reflect(bind=engine)
-    return engine, metadata
+    # metadata = MetaData()
+    # metadata.reflect(bind=engine)
+    return engine#, metadata
 
 def get_pk(table_name: str):
     """
@@ -30,7 +30,12 @@ def get_pk(table_name: str):
     return pk_dict[table_name]
 
 
-engine, metadata = get_db_connection()
+#engine, metadata = get_db_connection()
+sqlite_file_name = "safety2.sqlite"
+sqlite_url = f"sqlite:///{sqlite_file_name}?mode=ro"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+#engine= get_db_connection()
 
 
 def get_session():
@@ -41,24 +46,54 @@ def get_session():
     finally:
         session.close()
 
-
-class Incident(SQLModel, table=True):
-    incident_rec_id: int = Field(primary_key=True)
-    Collision_Report_Number: str = Field()
-    incident_date: str = Field()
+class IncidentBase(SQLModel):
     City_Name: str = Field()
+    County_Name: str = Field()
+    Latitude: float = Field()
+    Longitude: float = Field()
 
-    vehicles: List["Vehicle"] = Relationship(back_populates="incident")
+class Incident(IncidentBase, table=True):
+    incident_rec_id: int = Field(primary_key=True)
+    City_Name: str = Field()
+    # Collision_Report_Number: str = Field()
+    Incident_Date: str = Field()
 
-class Vehicle(SQLModel, table=True):
-    vehicle_rec_id: int = Field(primary_key=True)
+    vehicles: list["Vehicle"] = Relationship(back_populates="incident")
+
+class IncidentCreate(IncidentBase):
+    pass
+
+class IncidentPublic(IncidentBase):
+    incident_rec_id: int
+    
+
+class VehicleBase(SQLModel):
     unit_number: int = Field()
     Vehicle_Type: str = Field()
 
-    incident_rec_id: Optional[int] = Field(default=None, foreign_key="incident.incident_rec_id")
-    #Collision_Report_Number: Optional[str] = Field(default=None, foreign_key="Incident.Collision_Report_Number")
-    incident: Optional[Incident] = Relationship(back_populates="vehicles")
+    # incident_rec_id: Optional[int] = Field(default=None, foreign_key="incident.incident_rec_id")
+    incident_rec_id: int | None = Field(default=None, foreign_key="incident.incident_rec_id")
 
+class Vehicle(VehicleBase, table=True):
+    vehicle_rec_id: int | None = Field(primary_key=True)
+    #Collision_Report_Number: Optional[str] = Field(default=None, foreign_key="Incident.Collision_Report_Number")
+    # incident: Optional[Incident] = Relationship(back_populates="vehicles")
+    incident: Incident | None = Relationship(back_populates="vehicles")
+
+
+class VehiclePublic(VehicleBase):
+    vehicle_rec_id: int
+
+
+class VehiclePublicWithIncident(VehiclePublic):
+    incident: IncidentPublic | None = None
+
+
+class IncidentPublicWithVehicles(IncidentPublic):
+    #incident_rec_id: int
+    # Incident_Date: str
+    # vehicles: List["Vehicle"] = Relationship(back_populates="incident")
+    vehicles: list[VehiclePublic] = []
 
 app  = FastAPI()
 
@@ -75,71 +110,16 @@ async def get_incident(Collision_Report_Number: str):
         return incidents
 
 
-@app.get("/incidents_by_city/{city_name}", response_model=List[Incident])
-async def get_incidents_by_city(city_name: str):
-    with Session(engine) as session:
-        statement = select(Incident).where(Incident.City_Name == city_name)
-        results = session.exec(statement)
-        return results.all()
+# @app.get("/incidents_by_city/{city_name}", response_model=list[IncidentPublicWithVehicles])
+# async def get_incidents_by_city(city_name: str):
+#     with Session(engine) as session:
+#         statement = select(Incident).where(Incident.City_Name == city_name)
+#         results = session.exec(statement)
+#         return results.all()
     
-# @app.get("/vehicles/{collision_report_number}")
-# async def get_vehicles_by_id(collision_report_number: str):
-
-#     with get_db_connection() as s_conn:
-#         try:
-#             qry = f"""
-#                     select i.Collision_report_number, i.incident_date
-#                     from incidents as i
-#                     where i.Collision_Report_number = ?
-#                 """
-#             df_incident = pd.read_sql(qry, s_conn, params=[collision_report_number])
-            
-#             qry = f"""
-#                 select v.unit_number, v.Vehicle_Type 
-#                 from incidents as i 
-#                     join vehicles as v on i.incident_rec_id = v.incident_rec_id 
-#                     where i.Collision_Report_number = ?
-#                 """
-#             df_vehicles = pd.read_sql(qry, s_conn, params=[collision_report_number])
-            
-#             structured_data = {
-#                 'incident': df_incident.to_dict(orient='records')[0],
-#                 'vehicles': df_vehicles.to_dict(orient='records')
-#             }
-            
-#             return structured_data
-
-#         except Exception as e:
-#             raise HTTPException(status_code=500, detail="Error retrieving vehicle data")
-    
-
-# @app.get("/persons/{collision_report_number}")
-# async def get_persons_by_id(collision_report_number: str):
-
-#     with get_db_connection() as s_conn:
-#         try:
-
-#             qry_incident = f"""
-#                 select i.Collision_report_number, i.incident_date
-#                 from incidents as i
-#                 where i.Collision_Report_number = ?
-#                 """
-#             df_incident = pd.read_sql(qry_incident, s_conn, params=[collision_report_number])
-            
-#             qry_person = f"""
-#                 select p.Involved_Person_Type, p.Age, p.Gender
-#                 from incidents as i 
-#                     join vehicles as v on i.incident_rec_id = v.incident_rec_id 
-#                     join persons as p on v.vehicle_rec_id = p.vehicle_rec_id
-#                 where i.Collision_Report_number = ?
-#                 """
-#             df_person = pd.read_sql(qry_person, s_conn, params=[collision_report_number])
-
-#             structured_data = {
-#                 'incident': df_incident.to_dict(orient='records')[0],
-#                 'persons': df_person.to_dict(orient='records')
-#             }
-#             return structured_data
-        
-#         except Exception as e:
-#             raise HTTPException(status_code=500, detail="Error retrieving person data")
+@app.get("/incidents_by_city/{City_Name}", response_model=IncidentPublicWithVehicles)
+def get_incidents_by_city(*, City_Name: str, session: Session = Depends(get_session)):
+    results = session.exec(select(Incident).where(Incident.City_Name==City_Name)).first()
+    if not results:
+        raise HTTPException(status_code=404, detail="No Incidents found")
+    return results
